4bd99ce7e03c6b39faf3b9ab7e90a80d5f9f2011
Where-Does-The-Money-Go <nate.schweizer@gmail.com>
2021-02-15 05:57:22 -0700

    updates to dash and pd plots

--- Plotly-Dash/callbacks.py
+++ Plotly-Dash/callbacks.py
@@ -10,37 +10,39 @@ import dash_html_components as html
 import pandas as pd
 import pickle
 from sklearn.inspection import plot_partial_dependence
+from mlxtend.regressor import StackingCVRegressor
 # Read the picked data frame from the project folder
 df = pd.read_pickle("app.data")
-df_ammenity = pd.read_pickle("ammenity.data")
+df_amenity = pd.read_pickle("ammenity.data")
+print(df_amenity.columns)
 stack= pickle.load(open('finalized_model_ri.sav', 'rb'))
 stackO= pickle.load(open('finalized_model_o.sav', 'rb'))
 df_ud = pd.read_csv("l2_detailed_listings.csv", encoding = "UTF-8")
 
-# CARD_KEYS = ['Rental Income', 'Occupancy']
-
-# ALLOWED_TYPES = df_ud.columns.tolist()
-
-# #Occupancy and Rental_Income Outputs
-# @app.callback(
-#     [Output(key +'_card', 'children') for key in CARD_KEYS],
-#     [Input("input_{}".format(_), "value") for _ in ALLOWED_TYPES],
-    
-#     pred = {[]}
-# def update_card_value(value):
-    
-
-#     state_df = df.loc[df.state==value].copy()
-#     state_df['new_cases'] = state_df['cases'].diff()
-#     state_df['new_deaths'] = state_df['deaths'].diff()
-#     correlations = state_df.corr().loc[:'residential', 'new_cases']
-#     return [dbc.Card(dbc .CardBody(
-#         [
-#             html.H5(key.title(), className="card-title"),
-#             html.P("Correlation: " + str(correlations[key]), className="card-text",
-#             ),
-#         ]
-#     ), color="danger" if correlations[key] < 0 else "success") for key in CARD_KEYS]               
+CARD_KEYS = ['Rental Income', 'Occupancy']
+Amenity_Names = df_amenity.columns.tolist()
+ALLOWED_TYPES = df_ud.columns.tolist()
+for name in Amenity_Names:
+    print(name)
+    if name in ALLOWED_TYPES:
+        ALLOWED_TYPES.remove(name)
+ALLOWED_TYPES.remove("occupancy")
+ALLOWED_TYPES.remove("rental_income") 
+pred = pd.DataFrame(columns=ALLOWED_TYPES)
+#Occupancy and Rental_Income Outputs
+@app.callback(
+    [Output('Rental Income', 'children')],
+    [Input("{}".format(_), "value") for _ in ALLOWED_TYPES])
+def update_card_value(id,value):
+    pred[1,id] = value
+    ri = stack.predict(pred)
+    return dbc.Card(dbc .CardBody(
+         [
+             html.H5(key.title(), className="card-title"),
+             html.P("Predicted Rental Income: " + str(ri), className="card-text",
+             ),
+         ]
+     ), color= "success")
 
 # @app.callback(
 #     [Output('line_chart', 'figure'),
@@ -67,7 +69,6 @@ df_ud = pd.read_csv("l2_detailed_listings.csv", encoding = "UTF-8")
 #            partial_O \
 
 
-# @app.callback(
 #     [Output(key +'_card', 'children') for key in CARD_KEYS],
 #     [Input('state-dropdown', 'value')])
 # def update_card_value(value):
--- Plotly-Dash/layouts.py
+++ Plotly-Dash/layouts.py
@@ -4,7 +4,7 @@ Define the layouts of each page/url
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
-from callbacks import df, df_ammenity, df_ud # import the df loaded from callbacks so we don't need to load it again
+from callbacks import df, df_amenity, df_ud # import the df loaded from callbacks so we don't need to load it again
 
 # the layout of homepage
 homepage_layout = html.Div(
@@ -58,7 +58,7 @@ SIDEBAR_STYLE = {
 # save all the parameters of the pages for easy accessing
 PAGES = [
     {'children': 'Home', 'href': '/', 'id': 'home'},
-    {'children': 'Correlation', 'href': '/correlation', 'id': 'correlation-page'}
+    {'children': 'Predictor', 'href': '/predictor', 'id': 'predictor-page'}
 ]
 
 # the layout of the sidebar
@@ -86,174 +86,221 @@ CARD_KEYS = ['Rental Income', 'Occupancy']
 
 # the layout of the correlation page
 predictor_layout = html.Div(children=[
-        dcc.Checklist(
+        html.H3(
+            'Predictor',
+            style={'text-align': 'center'}
+        ),
+        html.H6(
+            'Amenities',
+            style={'text-align': 'center'}
+        ),
+        dcc.Dropdown(
             id='amenity-checkbox',
             options=
-                [{"label": amenity, "value": amenity} for amenity in df_ammenity.columns]
+                [{"label": amenity, "value": amenity} for amenity in df_amenity.columns]
             ,
-            value='Dishwasher'
+            value='Dishwasher',
+            multi = True,
+            persistence = True,
         ),
-        dcc.Dropdown(
-            id='neighborhood-dropdown',
-            options=
-                [{"label": neighborhood, "value": neighborhood} for neighborhood in df_ud.neighborhood.unique()]
-            ,
-            value='University City',
-            clearable=False,
-            style={"width": 100}
-        ),
-        dcc.Dropdown(
-            id='bookable-dropdown',
-            options=
-                [{"label":instant_book_enabled, "value": instant_book_enabled} for instant_book_enabled in df_ud.instant_book_enabled.unique()]
-            ,
-            value='True',
-            clearable=False,
-            style={"width": 100}
-        ),
-        dcc.Dropdown(
-            id='cancelation-dropdown',
-            options=
-                [{"label":cancellation_policy, "value": cancellation_policy} for cancellation_policy in df_ud.cancellation_policy.unique()]
-            ,
-            value='University City',
-            clearable=False,
-            style={"width": 100}
-        ),
-        dcc.Dropdown(
-            id='property-dropdown',
-            options=
-                [{"label":property_type, "value": property_type} for property_type in df_ud.property_type.unique()]
-            ,
-            value='House',
-            clearable=False,
-            style={"width": 100}
-        ),
-        dcc.Input(
-            id="price", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="bedrooms", 
-            type="number",
-            max=4,
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="beds", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="bathrooms", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="min_nights", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="person_capacity", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="reviews_count", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="picture_count", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="check_in_time", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="check_out_time", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="guests_included", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="security_deposit_native", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="star_rating", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="price_for_extra_person_native", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="cleaning_fee_native", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="listing_weekend_price_native", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="monthly_price_factor", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-        dcc.Input(
-            id="weekly_price_factor", 
-            type="number",
-            debounce=True, placeholder="Debounce True"
-        ),
-
+        dbc.Row([
+            dbc.Col([
+                html.Div(
+                    [
+                        html.H6("Neighborhood",
+                            style={'text-align': 'left'}),
+                        dcc.Dropdown(
+                            id='neighborhood',
+                            options=
+                                [{"label": neighborhood, "value": neighborhood} for neighborhood in df_ud.neighborhood.unique()]
+                            ,
+                            value='University City',
+                            clearable=False,
+                            style={"width": 100},
+                        ),
+                        html.H6("Instant Book Enabled?",
+                            style={'text-align': 'left'}),
+                        dcc.Dropdown(
+                            id='instant_book_enabled',
+                            options=
+                                [{"label":instant_book_enabled, "value": instant_book_enabled} for instant_book_enabled in df_ud.instant_book_enabled.unique()]
+                            ,
+                            value='True',
+                            clearable=False,
+                            style={"width": 100}
+                        ),
+                        html.H6("Cancellation Policy",
+                            style={'text-align': 'left'}),
+                        dcc.Dropdown(
+                            id='cancellation_policy',
+                            options=
+                                [{"label":cancellation_policy, "value": cancellation_policy} for cancellation_policy in df_ud.cancellation_policy.unique()]
+                            ,
+                            value='University City',
+                            clearable=False,
+                            style={"width": 100},
+                            optionHeight = 50
+                        ),
+                        html.H6("Property Type",
+                            style={'text-align': 'left'}),
+                        dcc.Dropdown(
+                            id='property_type',
+                            options=
+                                [{"label":property_type, "value": property_type} for property_type in df_ud.property_type.unique()]
+                            ,
+                            value='House',
+                            clearable=False,
+                            style={"width": 100}
+                        ),
+                    ],
+                    style={"width": "50%"},
+                ),
+            ]),
+            dbc.Col([
+                html.H6("Price",
+                    style={'text-align': 'left'}),
+                dcc.Input(
+                    id="price", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Weekend Price",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="listing_weekend_price_native", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Cleaning Fee",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="cleaning_fee_native", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Weekly Discount",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="weekly_price_factor", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Monthly Discount",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="monthly_price_factor", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Security Deposit",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="security_deposit_native", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Number of Guests Included",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="guests_included", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Fee for Additional Guests (per person)",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="price_for_extra_person_native", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),    
+            ]),
+            dbc.Col([
+                html.H6("Beds",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="beds", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Bathrooms",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="bathrooms", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Bedrooms",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="bedrooms", 
+                    type="number",
+                    max=4,
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Maximum Guest Capacity",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="person_capacity", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+            ]),
+            dbc.Col([
+                html.H6("Minimum Nights per Stay",
+                    style={'text-align': 'justify'}),
+                dcc.Input(
+                    id="min_nights", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("# of Reviews",
+                    style={'text-align': 'justify'}
+                ),
+                dcc.Input(
+                    id="reviews_count", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("# of Pictures",
+                    style={'text-align': 'justify'}
+                ),
+                dcc.Input(
+                    id="picture_count", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Check In Time",
+                    style={'text-align': 'justify'}
+                ),
+                dcc.Input(
+                    id="check_in_time", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Check Out Time",
+                    style={'text-align': 'justify'}
+                ),
+                dcc.Input(
+                    id="check_out_time", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+                html.H6("Star Rating",
+                    style={'text-align': 'justify'}
+                ),
+                dcc.Input(
+                    id="star_rating", 
+                    type="number",
+                    debounce=True, placeholder="Debounce True"
+                ),
+            ]),
+        ]),
         html.Hr(),
         dbc.Row(
             [
-                dbc.Col([], id=key+'_card') for key in CARD_KEYS
+                dbc.Col([], id='Rental Income')
             ],
             className="mb-4",
-        ),
-
-        html.H3(
-            'Daily Confirmed Cases',
-            style={'text-align': 'center'}
-        ),
-        dcc.Graph(
-            id='line_chart',
-            style={
-                'width': '80%',
-                'height': '500px',
-                'text-align': 'center',
-                'margin': 'auto'
-            }
-        ),
-        html.H3(
-            'Percentage of Change in Mobility',
-            style={'text-align': 'center'}
-        ),
-        dcc.Graph(
-            id='trend_chart',
-            style={
-                'width': '80%',
-                'height': '500px',
-                'text-align': 'center',
-                'margin': 'auto'
-            }
         ),
 ])
 
